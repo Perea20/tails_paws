2182276a6f5074b06397b8ba5119e9e20b431932
J.Perea <jotape.09.21@gmail.com>
2026-05-05 01:43:03 +0200

    fixing test directory

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_it_works(): void
+    {
+        $this->assertTrue(true);
+    }
+}
